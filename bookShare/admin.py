646e126517886bb8713e163d2ac783300c5185b7
Ross Smith <2652064S@student.gla.ac.uk>
2023-03-05 20:18:28 +0000

    Added core login/register/logout functionality

--- bookShare/admin.py
+++ bookShare/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from bookShare.models import UserProfile, Author, Publisher
 
-# Register your models here.
+admin.site.register(UserProfile)
+admin.site.register(Author)
+admin.site.register(Publisher)
--- bookShare/views/register.py
+++ bookShare/views/register.py
@@ -1,4 +1,47 @@
-from django.shortcuts import render
+import datetime
+from bookShare.forms import UserForm, UserProfileForm
+from django.shortcuts import render, redirect
+from django.urls import reverse
+
+DEFAULT_REPUTATION = 0
 
 def register(request):
-    return render(request,'bookShare/register.html')
+    if request.user.is_authenticated:
+        return redirect(reverse('bookShare:browse')) # Already signed in.
+    
+    registered = False
+
+
+    if request.method == 'POST':
+        user_form = UserForm(request.POST)
+        profile_form = UserProfileForm(request.POST)
+
+        if user_form.is_valid() and profile_form.is_valid():
+            user = user_form.save()
+
+            user.set_password(user.password)
+            user.save()
+
+            profile = profile_form.save(commit=False)
+            profile.user = user
+            profile.reputation = DEFAULT_REPUTATION
+            profile.joined_date = datetime.date.today
+
+            if 'user_image' in request.FILES:
+                profile.user_image = request.FILES['user_image']
+
+            profile.save()
+
+            registered = True
+        else:
+            print(user_form.errors, profile_form.errors)
+    else:
+        user_form = UserForm()
+        profile_form = UserProfileForm()
+
+    
+    return render(request,
+        'bookShare/register.html',
+        context = {'user_form': user_form,
+        'profile_form': profile_form,
+        'registered': registered})
