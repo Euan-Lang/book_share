efc73b4c6fcac1e27ee352ab9bae0926817e8ae6
“Euan <2652072L@student.gla.ac.uk>
2023-03-05 22:43:17 +0000

    partial completion of models

--- bookShare/admin.py
+++ bookShare/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from bookShare.models import UserProfile, Author, Publisher
+from bookShare.models import UserProfile, Author, Publisher, Book
 
 admin.site.register(UserProfile)
 admin.site.register(Author)
 admin.site.register(Publisher)
+admin.site.register(Book)
