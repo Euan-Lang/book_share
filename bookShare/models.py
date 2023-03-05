4e04efc4bca7f890d4dd8ba4d77e415e0fa3d091
“Euan <2652072L@student.gla.ac.uk>
2023-03-05 22:43:17 +0000

    partial completion of models

--- bookShare/models.py
+++ bookShare/models.py
@@ -3,12 +3,19 @@ from django.contrib.auth.models import User
 import os
 
 
-def rename(instance, file_name):
+def renameUser(instance, file_name):
     extension = file_name.split('.')[-1]
     if instance.user.username:
         file_name = '{}.{}'.format(instance.user.username, extension)  
     
     return os.path.join('user/', file_name)
+
+def renameBook(instance, file_name):
+    extension = file_name.split('.')[-1]
+    if instance.book_id:
+        file_name = '{}.{}'.format(instance.book_id, extension)  
+    
+    return os.path.join('book/', file_name)
 
 class Author(models.Model):
     author_id = models.AutoField(primary_key=True)
@@ -25,10 +32,22 @@ class UserProfile(models.Model):
     phone_number = models.CharField(max_length=11)
     joined_date = models.DateField(auto_now_add=True)
     reputation = models.IntegerField()
-    user_image = models.ImageField(upload_to=rename,null=True,blank=True)
+    user_image = models.ImageField(upload_to=renameUser,null=True,blank=True)
+    follows = models.ManyToManyField("self",blank=True)
     
     def __str__(self):
         return self.user.username
 
     def __unicode__(self):
         return self.user.username
+    
+class Book(models.Model):
+    book_id = models.AutoField(primary_key=True)
+    user_profile = models.OneToOneField(UserProfile,on_delete=models.CASCADE)
+    publisher_id = models.ManyToManyField(Publisher)
+    isbn = models.IntegerField()
+    cover_image = models.ImageField(upload_to=renameBook,null=True,blank=True)
+    title = models.CharField(max_length=128)
+    genre = models.CharField(max_length=128)
+    upload_time = models.DateTimeField(auto_now_add= True)
+    is_reserved = models.BooleanField(default=False)
