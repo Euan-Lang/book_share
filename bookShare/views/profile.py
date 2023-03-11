a741ac4e14b3826abb3f33e88b369b45a1ad5e15
Ross Smith <2652064S@student.gla.ac.uk>
2023-03-11 17:00:19 +0000

    Fixed profile/added dynamic nav bar/made profile info template-wide

--- bookShare/views/profile.py
+++ bookShare/views/profile.py
@@ -7,16 +7,18 @@ from django.urls import reverse
 def profile(request, user_id):
     context = {}
 
-    # try:
-    user = User.objects.get(username = user_id)
-    user_profile = UserProfile.objects.get(user = user)
-    context["profile"] = user_profile
+    try:
+        user = User.objects.get(username = user_id)
+        user_profile = UserProfile.objects.get(user = user)
+        context["profile"] = user_profile
 
-    context["following"] = Follows.objects.filter(follower = user_profile)
-    context["followers"] = Follows.objects.filter(following=user_profile)
-    context["is_following"] = context["followers"].filter(follower=UserProfile.objects.get(user=request.user))
-
-    # except :
-        # return redirect(reverse('bookShare:browse'))
+        context["following"] = Follows.objects.filter(follower = user_profile)
+        context["followers"] = Follows.objects.filter(following=user_profile)
+        if request.user.is_authenticated and UserProfile.objects.filter(user=request.user).exists():
+            context["is_following"] = context["followers"].filter(follower=UserProfile.objects.get(user=request.user))
+        else:
+            context["is_following"] = "invalid"
+    except:
+        return redirect(reverse('bookShare:browse'))
 
     return render(request,'bookShare/profile.html', context=context)
