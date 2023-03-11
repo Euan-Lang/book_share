6f66e58eefb3f71da19921fdfca86032cff41c67
Pieter Van Tuijl <pietervantuijl2000@gmail.com>
2023-03-11 21:53:16 +0000

    pieter: create login and register page + header layout
    Merge commit with main

--- bookShare/forms.py
+++ bookShare/forms.py
@@ -4,11 +4,13 @@ from django import forms
 
 
 class UserForm(forms.ModelForm):
-    username = forms.CharField(label="username", 
-                               widget=forms.TextInput(attrs={"placeholder":"your username"}))
+    username = forms.CharField(label="username",
+                               widget=forms.TextInput(attrs={"placeholder": "your username"}))
     email = forms.EmailField(label="email",
-                             widget=forms.EmailInput(attrs={"placeholder":"yourname@example.com"}))
-    password = forms.CharField(widget=forms.PasswordInput(attrs={"placeholder":"********"}))
+                             widget=forms.EmailInput(attrs={"placeholder": "yourname@example.com"}))
+    password = forms.CharField(widget=forms.PasswordInput(
+        attrs={"placeholder": "********"}))
+
     class Meta:
         model = User
         fields = ('username', 'email', 'password',)
@@ -16,11 +18,12 @@ class UserForm(forms.ModelForm):
 
 class UserProfileForm(forms.ModelForm):
     location = forms.CharField(label="location",
-                               widget=forms.TextInput(attrs={"placeholder":"Exampleville"}))
+                               widget=forms.TextInput(attrs={"placeholder": "Exampleville"}))
     post_code = forms.CharField(label="post_code",
-                                widget=forms.TextInput(attrs={"placeholder":"XXXXXX"}))
+                                widget=forms.TextInput(attrs={"placeholder": "XXXXXX"}))
     phone_number = forms.CharField(label="phone_number",
-                                   widget=forms.TextInput(attrs={"placeholder":"01234567901"}))
+                                   widget=forms.TextInput(attrs={"placeholder": "01234567901"}))
+
     class Meta:
         model = UserProfile
         fields = ('location', 'post_code', 'phone_number', 'user_image')
