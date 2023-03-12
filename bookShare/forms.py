76fe403b579b40d6977ef34ea6d2f99799cc33ea
Ross Smith <smithross2003@gmail.com>
2023-03-12 17:04:53 +0000

    Fixed register inputs and pfp changing

--- bookShare/forms.py
+++ bookShare/forms.py
@@ -5,9 +5,9 @@ from django import forms
 
 class UserForm(forms.ModelForm):
     username = forms.CharField(label="Username",
-                               widget=forms.TextInput(attrs={"placeholder": "your username"}))
+                               widget=forms.TextInput(attrs={"placeholder": "your username", "maxlength":"64"}))
     email = forms.EmailField(label="Email",
-                             widget=forms.EmailInput(attrs={"placeholder": "yourname@example.com"}))
+                             widget=forms.EmailInput(attrs={"placeholder": "yourname@example.com", "maxlength":"64"}))
     password = forms.CharField(widget=forms.PasswordInput(
         attrs={"placeholder": "********"}))
 
@@ -18,13 +18,13 @@ class UserForm(forms.ModelForm):
 
 class UserProfileForm(forms.ModelForm):
     location = forms.CharField(label="Location",
-                               widget=forms.TextInput(attrs={"placeholder": "Exampleville"}))
+                               widget=forms.TextInput(attrs={"placeholder": "Exampleville", "maxlength":"128"}))
     post_code = forms.CharField(label="Post Code",
-                                widget=forms.TextInput(attrs={"placeholder": "XXXXXX"}))
+                                widget=forms.TextInput(attrs={"placeholder": "XXXXXX", "maxlength":"6"}))
     phone_number = forms.CharField(label="Phone No.",
-                                   widget=forms.TextInput(attrs={"placeholder": "01234567901"}))
+                                   widget=forms.TextInput(attrs={"placeholder": "01234567901", "maxlength":"11"}))
     
-    user_image = forms.ImageField(label="Profile Picture")
+    user_image = forms.ImageField(label="Profile Picture", required=False)
 
     class Meta:
         model = UserProfile
