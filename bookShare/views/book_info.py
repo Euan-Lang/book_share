1fb5654812bb3e40c0d9559f189d06b76683d1cc
“Euan <2652072L@student.gla.ac.uk>
2023-03-14 13:47:48 +0000

    basic book profile

--- bookShare/views/book_info.py
+++ bookShare/views/book_info.py
@@ -1,4 +1,11 @@
 from django.shortcuts import render
+from bookShare.models import Book
+from django.shortcuts import redirect
+from django.urls import reverse
 
-def book_info(request, genre, book_id):
-    return render(request,'bookShare/book_info.html')
+def book_info(request, book_id):
+    context={}
+
+    book = Book.objects.get(book_id = book_id)
+    context['book'] =book
+    return render(request,'bookShare/book_info.html', context=context)
