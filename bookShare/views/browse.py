010433cff8cd4755ec2a3767ee2939d2cada877e
Scriptminer <aidanw@lochan.org>
2023-03-16 12:09:38 +0000

    Create browse page and dummy search
    Implement the outline of the basic functionality of the browse page.
    Search filter options yet to be implemented, and search currently just
    returns all books regardless of input.

--- bookShare/views/browse.py
+++ bookShare/views/browse.py
@@ -1,4 +1,13 @@
 from django.shortcuts import render
+from bookShare.models import Book
 
 def browse(request):
-    return render(request,'bookShare/browse.html')
+    context = {}
+
+    context["results"] = Book.objects.filter() # By default, show all books
+
+    if request.method == "POST": 
+        query = request.POST["query_text"]
+        context["results"] = Book.objects.filter()
+    
+    return render(request,'bookShare/browse.html', context=context)
