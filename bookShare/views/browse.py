5cc2e0c46d91e29e3118314d398c1760a58c29df
Scriptminer <aidanw@lochan.org>
2023-03-17 11:11:44 +0000

    Change population book authors, add search by user

--- bookShare/views/browse.py
+++ bookShare/views/browse.py
@@ -9,6 +9,6 @@ def browse(request):
 
     if request.method == "POST": 
         query = request.POST["query_text"]
-        context["results"] = Book.objects.filter(Q(title__icontains=query) | Q(author__icontains=query) | Q(isbn__icontains=query) | Q(genre__icontains=query) | Q(book_id__icontains=query) )
+        context["results"] = Book.objects.filter(Q(title__icontains=query) | Q(author__icontains=query) | Q(isbn__icontains=query) | Q(genre__icontains=query) | Q(book_id__icontains=query) | Q(user_profile__user__username__icontains=query))
     
     return render(request,'bookShare/browse.html', context=context)
