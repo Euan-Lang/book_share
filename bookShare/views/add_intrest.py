3f44e32c3946349411017d89cf84d748d7485abe
“Euan <euanlang2811@gmail.com>
2023-03-17 21:18:43 +0000

    fixed error with missing user profile

--- bookShare/views/add_intrest.py
+++ bookShare/views/add_intrest.py
@@ -1,5 +1,3 @@
-from django.shortcuts import render
-from django.contrib.auth.models import User
 from bookShare.models import Intrest, UserProfile, Book
 from django.shortcuts import redirect
 from django.urls import reverse
@@ -8,7 +6,7 @@ from django.contrib.auth.decorators import login_required
 @login_required(login_url="bookShare:login")
 def add_intrest(request,book_id):
     book = Book.objects.get(book_id=book_id)
-    if request.user.username != book.user_profile.user.username:
+    if request.user.username != book.user_profile.user.username and UserProfile.objects.filter(user = request.user).exists():
         user_profile = UserProfile.objects.get(user = request.user)
         if not Intrest.objects.filter(book_id=book_id,user_profile=user_profile).exists():
             Intrest.objects.create(book_id=book_id,user_profile=user_profile)
