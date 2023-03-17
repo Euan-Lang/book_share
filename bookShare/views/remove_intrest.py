777494e50a48af518ec296678810dbfc902c6911
“Euan <euanlang2811@gmail.com>
2023-03-17 21:13:49 +0000

    added remove_intrest

--- bookShare/views/remove_intrest.py
+++ bookShare/views/remove_intrest.py
@@ -7,4 +7,9 @@ from django.contrib.auth.decorators import login_required
 
 @login_required(login_url="bookShare:login")
 def remove_intrest(request,book_id):
-    pass
+    book = Book.objects.get(book_id=book_id)
+    user_profile = UserProfile.objects.get(user = request.user)
+    if Intrest.objects.filter(book = book,user_profile = user_profile).exists():
+        Intrest.objects.get(book = book,user_profile = user_profile).delete()
+
+    return redirect(reverse('bookShare:book_info', kwargs={'book_id':book_id}))
