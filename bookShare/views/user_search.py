0f5ec6b3ff656aa2173e6d69f371a871b29eaa59
Ross Smith <smithross2003@gmail.com>
2023-03-18 20:04:03 +0000

    Added Ajax to user search

--- bookShare/views/user_search.py
+++ bookShare/views/user_search.py
@@ -1,14 +1,15 @@
 from django.shortcuts import render
 from django.contrib.auth.models import User
 from bookShare.models import UserProfile
+from django.http import JsonResponse
 
 def user_search(request):
     context = {}
 
-    if request.method == "POST": 
-        username = request.POST["username"]
-        if not username == "":
-            context["results"] = UserProfile.objects.filter(user__in=User.objects.filter(username__icontains=username))
+    if request.is_ajax and request.method == "POST": 
+        username = request.POST.get("username", False)
+        results = UserProfile.objects.filter(user__in=User.objects.filter(username__icontains=username))
+        return JsonResponse({"instances":[result.user.username for result in results]}, status=200)
 
 
-    return render(request,'bookShare/user_search.html', context=context)
+    return render(request,'bookShare/user_search.html')
