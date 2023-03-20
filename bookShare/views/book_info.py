0303c080b72029217c1a4dd8fc7756ed285c0ab0
Ross Smith <smithross2003@gmail.com>
2023-03-20 22:46:37 +0000

    Fixed map for when postcode didn't exist

--- bookShare/views/book_info.py
+++ bookShare/views/book_info.py
@@ -5,18 +5,19 @@ from django.urls import reverse
 from bookShare.views.map_functions import getCoordsContextDict
 
 def book_info(request, book_id):
-    context= getCoordsContextDict(book_id)
-
     try:
-        book = Book.objects.get(book_id = book_id)
-        context['book'] = book
-        context["interested_users"] = Interest.objects.filter(book_id = book_id)
-        if request.user.is_authenticated and UserProfile.objects.filter(user=request.user).exists():
-            user_profile = UserProfile.objects.get(user = request.user)
-            context['interested'] = Interest.objects.filter(book_id = book_id,user_profile=user_profile).exists()
-        else:
-            context['interested'] = 'invalid'
+        context= getCoordsContextDict(book_id)
     except:
-        return redirect(reverse('bookShare:browse'))
+        context= {"invalid_map":True}
+
+    book = Book.objects.get(book_id = book_id)
+    context['book'] = book
+    context["interested_users"] = Interest.objects.filter(book_id = book_id)
+    if request.user.is_authenticated and UserProfile.objects.filter(user=request.user).exists():
+        user_profile = UserProfile.objects.get(user = request.user)
+        context['interested'] = Interest.objects.filter(book_id = book_id,user_profile=user_profile).exists()
+    else:
+        context['interested'] = 'invalid'
+    
     
     return render(request,'bookShare/book_info.html', context=context)
--- bookShare/views/profile.py
+++ bookShare/views/profile.py
@@ -8,19 +8,20 @@ from bookShare.views.map_functions import *
 def profile(request, user_id):
     context = {}
 
+    user = User.objects.get(username = user_id)
+    user_profile = UserProfile.objects.get(user = user)
     try:
-        user = User.objects.get(username = user_id)
-        user_profile = UserProfile.objects.get(user = user)
         context = formatContextDict(getCoordsRequest(user_profile.post_code))
-        context["profile"] = user_profile
-
-        context["following"] = Follows.objects.filter(follower = user_profile)
-        context["followers"] = Follows.objects.filter(following=user_profile)
-        if request.user.is_authenticated and UserProfile.objects.filter(user=request.user).exists():
-            context["is_following"] = context["followers"].filter(follower=UserProfile.objects.get(user=request.user)).exists()
-        else:
-            context["is_following"] = "invalid"
     except:
-        return redirect(reverse('bookShare:browse'))
+        context = {"invalid_map":True}
+    context["profile"] = user_profile
+
+    context["following"] = Follows.objects.filter(follower = user_profile)
+    context["followers"] = Follows.objects.filter(following=user_profile)
+    if request.user.is_authenticated and UserProfile.objects.filter(user=request.user).exists():
+        context["is_following"] = context["followers"].filter(follower=UserProfile.objects.get(user=request.user)).exists()
+    else:
+        context["is_following"] = "invalid"
+    
 
     return render(request,'bookShare/profile.html', context=context)
