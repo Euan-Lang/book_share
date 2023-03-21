94594e9bd57f76d7b23117acb0375133fcb21d30
Pieter Van Tuijl <pietervantuijl2000@gmail.com>
2023-03-21 23:10:41 +0000

    browsePage_pieter: initial browsing page UI setup

--- bookShare/views/browse.py
+++ bookShare/views/browse.py
@@ -2,37 +2,38 @@ from django.db.models import Q
 from django.http import JsonResponse
 from django.shortcuts import render
 
-from bookShare.models import Book
+from bookShare.models import Book, UserProfile
 
 from .map_functions import getCoordsRequest
 
+
 def browse(request):
     context = {}
-    print("ajax", request.is_ajax())
     if request.method == "POST":
         general_query = request.POST["general_query"]
-        genre_query = request.POST["genre_query"]
-        publisher_query = request.POST["publisher_query"]
-        author_query = request.POST["author_query"]
+        # genre_query = request.POST["genre_query"]
+        # publisher_query = request.POST["publisher_query"]
+        # author_query = request.POST["author_query"]
         try:
             max_radius_query = int(request.POST["max_radius_query"])
         except ValueError:
-            max_radius_query = 100_000_000 # Effectively infinity if no or incorrect input
+            max_radius_query = 100_000_000  # Effectively infinity if no or incorrect input
         postcode = request.POST["postcode"]
         available_only = True if request.POST["available_only"] == "true" else False
         sort_order = request.POST["sort"]
         print(max_radius_query, available_only)
-        
+
         # Select all books which contain the general query in any field
-        results = Book.objects.filter(Q(title__icontains=general_query) | Q(author__icontains=general_query) | Q(isbn__icontains=general_query) | Q(genre__icontains=general_query) | Q(book_id__icontains=general_query) | Q(user_profile__user__username__icontains=general_query))
+        results = Book.objects.filter(Q(title__icontains=general_query) | Q(author__icontains=general_query) | Q(isbn__icontains=general_query) | Q(
+            genre__icontains=general_query) | Q(book_id__icontains=general_query) | Q(user_profile__user__username__icontains=general_query))
         # Filter to remove all books not containing the filter queries in their relevant fields
-        results = results.filter(genre__icontains=genre_query)
-        results = results.filter(publisher__icontains=publisher_query)
-        results = results.filter(author__icontains=author_query)
+        # results = results.filter(genre__icontains=genre_query)
+        # results = results.filter(publisher__icontains=publisher_query)
+        # results = results.filter(author__icontains=author_query)
         if available_only:
             results = results.filter(is_reserved__exact=False)
 
-        if(postcode):
+        if (postcode):
             location = getCoordsRequest(postcode)
             valid_postcode = True if location["status"] == "match" and location["match_type"] == "unit_postcode" else False
             print("Coords!", getCoordsRequest(postcode))
@@ -42,11 +43,19 @@ def browse(request):
         # Sort results into correct order
         results = sort_results(results, sort_order)
         context["results"] = results
-        return JsonResponse({"results_container":render(request,'bookShare/browse_results.html', context=context).content.decode("utf-8"), "valid_postcode": valid_postcode})
-
+        return JsonResponse({"results_container": render(request, 'bookShare/browse_results.html', context=context).content.decode("utf-8"), "valid_postcode": valid_postcode})
 
     else:
-        return render(request,'bookShare/browse.html', context=context)
+        context["dropdowns"] = {
+            "genres": Book.objects.values_list("genre").distinct(),
+            "authors": Book.objects.values_list("author").distinct(),
+            "publishers": Book.objects.values_list("publisher").distinct(),
+        }
+        context["user_postcode"] = UserProfile.objects.get(
+            user=request.user).post_code
+        # print("hello", request.user.post_code)
+        return render(request, 'bookShare/browse.html', context=context)
+
 
 def sort_results(query_set, sort_order):
     orderings = {
--- static/js/ajax-browse-general-query.js
+++ static/js/ajax-browse-general-query.js
@@ -1,42 +1,71 @@
+import { debounce } from "./utils.js";
 
-$(document).ready(function(e) {
-    $('#book_search_form').on("submit", submitHandler);
-    $('#filter_search_form').on("input", submitHandler);
-    $('#sort_form').on("input", submitHandler);
-    submitHandler(null); // Load in results initially
+$(document).ready(function (e) {
+  $("#book_search_form").on("submit", submitHandler);
+  $("#filter_search_form").on("input", debounce(submitHandler, 200));
+  $("#sort_form").on("input", debounce(submitHandler, 200));
+  submitHandler(null); // Load in results initially
+
+  document.querySelectorAll(".dropdownCheckboxButton").forEach((dropdown) => {
+    dropdown.onclick = function (e) {
+      e.preventDefault();
+      const { dropdownType, dropdownClass } = e.target.dataset;
+      const dropdown = document.querySelector(
+        `#${dropdownType}_${dropdownClass}`
+      );
+      if (dropdown) {
+        dropdown.classList.toggle("hidden");
+      }
+    };
+  });
 });
 
 function submitHandler(e) {
-    if (e) {
-        e.preventDefault();
+  if (e) {
+    e.preventDefault();
+  }
+  var fields = [
+    "general_query",
+    // "genre_query",
+    // "publisher_query",
+    // "author_query",
+    "max_radius_query",
+    "postcode",
+    "available_only",
+    "sort",
+  ];
+
+  var data = { csrfmiddlewaretoken: csrftoken };
+  console.log($("#available_only_field").is(":checked"));
+
+  fields.forEach((field) => {
+    if (field == "available_only") {
+      data[field] = $("#available_only_field").is(":checked");
+    } else {
+      data[field] = $("#" + field + "_field").val();
     }
-    var fields = ["general_query","genre_query","publisher_query","author_query","max_radius_query","postcode","available_only","sort"]
-    
-    var data = {"csrfmiddlewaretoken":csrftoken};
-    for (var field in fields) {
-        data[fields[field]] = $("#"+fields[field]+"_field").val();
-    }
-    $("#valid_postcode").html("&#8634");
-    $.ajax( {
-        type: 'POST',
-        url: '/bookShare/browse/',
-        data: data,
-        success: function (response) {
-            $("#book_search_form")[0].reset(); // Clear input field on search
-            $("#results_container").html(response["results_container"]);
-            if (response["valid_postcode"]) {
-                $("#valid_postcode").html("&#10003");
-            } else {
-                $("#valid_postcode").html("&#10007");
-            }
-        }
-    });
+  });
+  $("#valid_postcode").html("&#8634");
+  $.ajax({
+    type: "POST",
+    url: "/bookShare/browse/",
+    data: data,
+    success: function (response) {
+      // $("#book_search_form")[0].reset(); // Clear input field on search
+      $("#results_container").html(response["results_container"]);
+      if (response["valid_postcode"]) {
+        $("#valid_postcode").html("&#10003");
+      } else {
+        $("#valid_postcode").html("&#10007");
+      }
+    },
+  });
 }
 
 function searchAuthor(e, author) {
-    e.preventDefault();
-    $("#book_search_form")[0].reset();
-    $("#filter_search_form")[0].reset();
-    $("#author_query_field").val(author);
-    submitHandler(null);
+  e.preventDefault();
+  $("#book_search_form")[0].reset();
+  $("#filter_search_form")[0].reset();
+  $("#author_query_field").val(author);
+  submitHandler(null);
 }
