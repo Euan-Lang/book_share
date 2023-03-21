a903475db3887c92d7e4e895743cac5c63f74125
Ross Smith <smithross2003@gmail.com>
2023-03-21 16:02:29 +0000

    Fix map conflict

--- static/js/mapBonus.js
+++ static/js/mapBonus.js
@@ -1,6 +1,26 @@
-setTimeout(onFinish,1500)
-function onFinish() {
+setTimeout(resizeMap, 500);
 
-    document.getElementById("map").style.height="360px";
-    // document.getElementById("map").style.width="480px";
+function resizeMap() {
+  const map = document.getElementById("map");
+  map.style.height = "360px";
+
+  // dynamically set width in case map is juxtaposed with sidebar
+  const mapContainer = document.querySelector(".mapSection");
+  const mapSideBar = mapContainer?.querySelector(".mapSidebar");
+  if (mapContainer && mapSideBar) {
+    const mapContainerWidth = mapContainer.getBoundingClientRect().width;
+    const mapSideBarWidth = mapSideBar?.getBoundingClientRect().width;
+    let mapWidth;
+
+    // from md breakpoint and smaller, map width is 100% of container
+    const screenWidth = window.screen.width;
+    if (screenWidth < 768) {
+      mapWidth = mapContainerWidth;
+    } else {
+      mapWidth = mapContainerWidth - mapSideBarWidth;
+    }
+    map.style.width = mapWidth + "px";
+  }
 }
+
+window.onresize = resizeMap;
