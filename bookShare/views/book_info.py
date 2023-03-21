ca9894b7744c5677b2e9763d8b58cfc220849aad
Pieter Van Tuijl <pietervantuijl2000@gmail.com>
2023-03-21 15:13:15 +0000

    book_info_page_2: finalize book info page styling
    Update CSS bundle file

--- bookShare/views/book_info.py
+++ bookShare/views/book_info.py
@@ -4,20 +4,27 @@ from django.shortcuts import redirect
 from django.urls import reverse
 from bookShare.views.map_functions import getCoordsContextDict
 
+
 def book_info(request, book_id):
     try:
-        context= getCoordsContextDict(book_id)
+        context = getCoordsContextDict(book_id)
     except:
-        context= {"invalid_map":True}
+        context = {"invalid_map": True}
 
-    book = Book.objects.get(book_id = book_id)
+    book = Book.objects.get(book_id=book_id)
     context['book'] = book
-    context["interested_users"] = Interest.objects.filter(book_id = book_id)
+    if book.is_reserved:
+        context["interested_users"] = Interest.objects.filter(book_id=book_id).filter(
+            user_profile__user__username=book.reserved_user.user.username)
+    else:
+        context["interested_users"] = Interest.objects.filter(book_id=book_id)
     if request.user.is_authenticated and UserProfile.objects.filter(user=request.user).exists():
-        user_profile = UserProfile.objects.get(user = request.user)
-        context['interested'] = Interest.objects.filter(book_id = book_id,user_profile=user_profile).exists()
+        user_profile = UserProfile.objects.get(user=request.user)
+        context['interested'] = Interest.objects.filter(
+            book_id=book_id, user_profile=user_profile).exists()
+        context['user_books'] = Book.objects.filter(
+            user_profile__user__username=book.user_profile.user.username).count()
     else:
         context['interested'] = 'invalid'
-    
-    
-    return render(request,'bookShare/book_info.html', context=context)
+
+    return render(request, 'bookShare/book_info.html', context=context)
--- static/js/mapDisplay.js
+++ static/js/mapDisplay.js
@@ -1,28 +1,27 @@
-
-
 function initMap() {
-
-    let latitudeValue = parseFloat(document.getElementById("latitude").textContent);
-    let longitudeValue = parseFloat(document.getElementById("longitude").textContent);
-    let centerLat = parseFloat(document.getElementById("centreLat").textContent);
-    let centreLon = parseFloat(document.getElementById("centreLon").textContent);
+  let latitudeValue = parseFloat(
+    document.getElementById("latitude").textContent
+  );
+  let longitudeValue = parseFloat(
+    document.getElementById("longitude").textContent
+  );
+  let centerLat = parseFloat(document.getElementById("centreLat").textContent);
+  let centreLon = parseFloat(document.getElementById("centreLon").textContent);
   const map = new google.maps.Map(document.getElementById("map"), {
     zoom: 12,
     center: { lat: centerLat, lng: centreLon },
     mapTypeId: "terrain",
   });
 
-
-    const obfsCircle = new google.maps.Circle({
-      strokeColor: "#FF0000",
-      strokeOpacity: 0.8,
-      strokeWeight: 2,
-      fillColor: "#FF0000",
-      fillOpacity: 0.35,
-      map,
-      center: {lat :latitudeValue, lng: longitudeValue},
-      radius: 1000,
-    });
-
+  const obfsCircle = new google.maps.Circle({
+    strokeColor: "#FF0000",
+    strokeOpacity: 0.8,
+    strokeWeight: 2,
+    fillColor: "#FF0000",
+    fillOpacity: 0.35,
+    map,
+    center: { lat: latitudeValue, lng: longitudeValue },
+    radius: 1000,
+  });
 }
 window.initMap = initMap;
