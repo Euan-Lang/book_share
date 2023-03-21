b7fc55e932c5bd7d7c816a78c59313cfae6fcf56
Ross Smith <smithross2003@gmail.com>
2023-03-21 17:54:06 +0000

    Made user search get all users at load

--- static/js/ajax-search.js
+++ static/js/ajax-search.js
@@ -1,3 +1,21 @@
+function regenerate_items(instances) {
+    if (instances.length == 0) {
+        $("#results-container").html("No users found.");
+        return;
+    }
+    $("#results-container").empty();
+    instances.forEach((user) => {
+        var username = user.username;
+        var image_url = user.image_url;
+        $("#results-container").append(`
+        <div class="transition-colors transition-transform hover:bg-[rgb(51,121,252)] hover:translate-y-1 hover:text-white bg-white shadow-lg rounded-xl h-[150px] w-[120px] lg:h-[200px] lg:w-[160px]">
+            <a href="/bookShare/${username}/profile/">
+                <img class="bg-white rounded-xl w-full h-4/5" src='${image_url ? image_url : "/static/images/default_pfp.jpg"}'/>
+                <div class="text-center align-middle sm:text-lg mt-1"> <b>${username}</b> </div>
+            </a>
+        </div>`)
+    })
+}
 
 $(document).ready(function() {
     $('#user-search-input').on("input", function(e) {
@@ -9,24 +27,21 @@ $(document).ready(function() {
             data: {"username":username, "csrfmiddlewaretoken":csrftoken},
             success: function (response) {
                 var instances = response["instances"];
-                if (instances.length == 0) {
-                    $("#results-container").html("No users found.");
-                    return;
-                }
-                $("#results-container").empty();
-                instances.forEach((user) => {
-                    var username = user.username;
-                    var image_url = user.image_url;
-                    $("#results-container").append(`
-                    <div class="transition-colors transition-transform hover:bg-[rgb(51,121,252)] hover:translate-y-1 hover:text-white bg-white shadow-lg rounded-xl h-[150px] w-[120px] lg:h-[200px] lg:w-[160px]">
-                        <a href="/bookShare/${username}/profile/">
-                            <img class="bg-white rounded-xl w-full h-4/5" src='${image_url ? image_url : "/static/images/default_pfp.jpg"}'/>
-                            <div class="text-center align-middle sm:text-lg mt-1"> <b>${username}</b> </div>
-                        </a>
-                    </div>`)
-                })
+                regenerate_items()
             }
         })
     });
+
+    // Get all at start
+    $.ajax( {
+        type: 'POST',
+        url: '/bookShare/search/',
+        data: {"username":"", "csrfmiddlewaretoken":csrftoken},
+        success: function (response) {
+            var instances = response["instances"];
+            regenerate_items(instances)
+        }
+    })
+
 });
     
