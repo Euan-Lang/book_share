d5412939557b5423100cd4a9e138e5efd5ba8920
Ross Smith <smithross2003@gmail.com>
2023-03-21 17:52:06 +0000

    Added styling to User Search

--- bookShare/views/user_search.py
+++ bookShare/views/user_search.py
@@ -9,7 +9,7 @@ def user_search(request):
     if request.is_ajax and request.method == "POST": 
         username = request.POST.get("username", False)
         results = UserProfile.objects.filter(user__in=User.objects.filter(username__icontains=username))
-        return JsonResponse({"instances":[result.user.username for result in results]}, status=200)
+        return JsonResponse({"instances":[{"username":result.user.username, "image_url": result.user_image.url if result.user_image else "none"} for result in results]}, status=200)
 
 
     return render(request,'bookShare/user_search.html')
--- static/js/ajax-search.js
+++ static/js/ajax-search.js
@@ -10,12 +10,20 @@ $(document).ready(function() {
             success: function (response) {
                 var instances = response["instances"];
                 if (instances.length == 0) {
-                    $("#results-container").html("<li>No users found.</li>");
+                    $("#results-container").html("No users found.");
                     return;
                 }
                 $("#results-container").empty();
-                instances.forEach((username) => {
-                    $("#results-container").append(`<li><a href = '/bookShare/${username}/profile'>${username}</a></li>`);
+                instances.forEach((user) => {
+                    var username = user.username;
+                    var image_url = user.image_url;
+                    $("#results-container").append(`
+                    <div class="transition-colors transition-transform hover:bg-[rgb(51,121,252)] hover:translate-y-1 hover:text-white bg-white shadow-lg rounded-xl h-[150px] w-[120px] lg:h-[200px] lg:w-[160px]">
+                        <a href="/bookShare/${username}/profile/">
+                            <img class="bg-white rounded-xl w-full h-4/5" src='${image_url ? image_url : "/static/images/default_pfp.jpg"}'/>
+                            <div class="text-center align-middle sm:text-lg mt-1"> <b>${username}</b> </div>
+                        </a>
+                    </div>`)
                 })
             }
         })
