4d2ffdf24bdd01fd4a0b5d2a2e91a7812e344864
Scriptminer <aidanw@lochan.org>
2023-03-21 01:10:23 +0000

    Add framework for sidebar.

--- bookShare/views/browse.py
+++ bookShare/views/browse.py
@@ -8,7 +8,7 @@ def browse(request):
     context["results"] = Book.objects.all() # By default, show all books
 
     if request.method == "POST": 
-        query = request.POST["query_text"]
+        query = request.POST["general_query"]
         context["results"] = Book.objects.filter(Q(title__icontains=query) | Q(author__icontains=query) | Q(isbn__icontains=query) | Q(genre__icontains=query) | Q(book_id__icontains=query) | Q(user_profile__user__username__icontains=query))
     
     return render(request,'bookShare/browse.html', context=context)
