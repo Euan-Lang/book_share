226aeb6f69a99c41227a73b8b3b9b093dff0e1f5
Pieter Van Tuijl <pietervantuijl2000@gmail.com>
2023-03-22 00:02:32 +0000

    browsePage_pieter: refactor search component in user search page

--- static/js/ajax-search.js
+++ static/js/ajax-search.js
@@ -1,47 +1,53 @@
 function regenerate_items(instances) {
-    if (instances.length == 0) {
-        $("#results-container").html("No users found.");
-        return;
-    }
-    $("#results-container").empty();
-    instances.forEach((user) => {
-        var username = user.username;
-        var image_url = user.image_url;
-        $("#results-container").append(`
+  if (instances.length == 0) {
+    $("#results-container").html("No users found.");
+    return;
+  }
+  $("#results-container").empty();
+  instances.forEach((user) => {
+    var username = user.username;
+    var image_url = user.image_url;
+    $("#results-container").append(`
         <div class="transition-colors transition-transform hover:bg-[rgb(51,121,252)] hover:translate-y-1 hover:text-white bg-white shadow-lg rounded-xl h-[150px] w-[120px] lg:h-[200px] lg:w-[160px]">
             <a href="/bookShare/${username}/profile/">
-                <img class="bg-white rounded-xl w-full h-4/5" src='${image_url != "none" ? image_url : "/static/images/default_pfp.jpg"}'>
+                <img class="bg-white rounded-xl w-full h-4/5" src='${
+                  image_url != "none"
+                    ? image_url
+                    : "/static/images/default_pfp.jpg"
+                }'>
                 <div class="text-center align-middle sm:text-lg mt-1"> <b>${username}</b> </div>
             </a>
-        </div>`)
-    })
+        </div>`);
+  });
 }
 
-$(document).ready(function() {
-    $('#user-search-input').on("input", function(e) {
-        e.preventDefault();
-        var username = $(this).val();
-        $.ajax( {
-            type: 'POST',
-            url: '/bookShare/search/',
-            data: {"username":username, "csrfmiddlewaretoken":csrftoken},
-            success: function (response) {
-                var instances = response["instances"];
-                regenerate_items(instances)
-            }
-        })
+$(document).ready(function () {
+  $("#user-search-input").on("input", function (e) {
+    e.preventDefault();
+    var username = $(this).val();
+    $.ajax({
+      type: "POST",
+      url: "/bookShare/search/",
+      data: { username: username, csrfmiddlewaretoken: csrftoken },
+      success: function (response) {
+        var instances = response["instances"];
+        regenerate_items(instances);
+      },
     });
+  });
 
-    // Get all at start
-    $.ajax( {
-        type: 'POST',
-        url: '/bookShare/search/',
-        data: {"username":"", "csrfmiddlewaretoken":csrftoken},
-        success: function (response) {
-            var instances = response["instances"];
-            regenerate_items(instances)
-        }
-    })
+  $("#user_search_form").on("submit", function (e) {
+    e.preventDefault();
+  });
 
+  // Get all at start
+  $.ajax({
+    type: "POST",
+    url: "/bookShare/search/",
+    data: { username: "", csrfmiddlewaretoken: csrftoken },
+    success: function (response) {
+      var instances = response["instances"];
+      regenerate_items(instances);
+    },
+  });
 });
-    
