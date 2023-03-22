862a1a2401cf6ed69c6d77781dec27af90eedd5b
Pieter Van Tuijl <pietervantuijl2000@gmail.com>
2023-03-22 11:49:33 +0000

    improvements: improve user search layout + followers layout

--- bookShare/views/user_search.py
+++ bookShare/views/user_search.py
@@ -3,13 +3,14 @@ from django.contrib.auth.models import User
 from bookShare.models import UserProfile
 from django.http import JsonResponse
 
+
 def user_search(request):
     context = {}
 
-    if request.is_ajax and request.method == "POST": 
+    if request.is_ajax and request.method == "POST":
         username = request.POST.get("username", False)
-        results = UserProfile.objects.filter(user__in=User.objects.filter(username__icontains=username))
-        return JsonResponse({"instances":[{"username":result.user.username, "image_url": result.user_image.url if result.user_image else "none"} for result in results]}, status=200)
+        results = UserProfile.objects.filter(
+            user__in=User.objects.filter(username__icontains=username))
+        return JsonResponse({"instances": [{"username": result.user.username, "location": result.location, "image_url": result.user_image.url if result.user_image else "none"} for result in results]}, status=200)
 
-
-    return render(request,'bookShare/user_search.html')
+    return render(request, 'bookShare/user_search.html')
